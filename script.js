24f269ddc5dfe841f6110a3f2de7e5d361a78f73
brandon-w1205 <bwu1205@gmail.com>
2022-08-19 14:51:52 -0700

    Adds a character constructor and variable piggy along with an addeventlistener to generate characters when clicking into the screen

--- script.js
+++ script.js
@@ -14,7 +14,43 @@ function drawBox(x, y, width, height, color) {
     ctx.fillRect(x, y, width, height)
 }
 
+// adds character constructor to create a character's hitbox
+class Character {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.alive = true;
+    }
+
+    render() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+let piggy = new Character(100, 100, 100, 100, "blue");
+
+
+
+
+
+
+
+
+
+
+
+// elements on page render when clicking into the screen to play, will add a button to prompt player later
 canvas.addEventListener('click', e => {
-    console.log(e.offsetX, e.offsetY);
-    drawBox(e.offsetX, e.offsetY, 30, 30, 'blue');
+    piggy.render()
 })
+
+
+
+
+
+
+
