0d621e67e9d56197dd498e93e650dccacb80b6d6
brandon-w1205 <bwu1205@gmail.com>
2022-08-19 11:32:27 -0700

    adds setAttribute for canvas for the height and width

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const canvas = document.querySelector('canvas');
 
 // sets context for canvas
 const ctx = canvas.getContext('2d');
+
+// set the canvas's resolution to be the same as the window
+canvas.setAttribute('height', getComputedStyle(canvas)['height'])
+canvas.setAttribute('width', getComputedStyle(canvas)['width'])
