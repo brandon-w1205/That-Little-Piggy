b2d4a3a08bb4c8585aa5a699d310a95d183765da
brandon-w1205 <bwu1205@gmail.com>
2022-08-19 16:58:28 -0700

    adds smooth left and right movement

--- script.js
+++ script.js
@@ -35,28 +35,88 @@ class Box {
 
 let piggy = new Box(100, 100, 100, 100, "blue");
 
-let wolf = new Box(1523, 60, 114, 710, 'grey');
+let wolf = new Box(1616, 55, 114, 710, 'grey');
 
-let wolfWall = new Box(1441, 0, 1, 770, 'white');
+let wolfWall = new Box(1570, 0, 1, 770, 'white');
 
-wolf.render();
-
-piggy.render();
-
-wolfWall.render();
+let ground = new Box(0, 715, 1720, 55, 'brown');
 
 
 
 
+let fps = 24
+
+
+
+let gravitySpeed = 4 * (60/fps)
+
+function gravity() {
+    if(piggy.y < 600 ) {
+        piggy.y += gravitySpeed;
+        // piggy.height += gravitySpeed;
+    }
+}
+
+
+
+function rightJump() {
+
+}
 
 // elements on page render when clicking into the screen to play, will add a button to prompt player later
 canvas.addEventListener('click', e => {
     console.log(e.offsetX, e.offsetY);
+    console.log(piggy.y)
 })
 
 
+const gameRefreshInterval = setInterval(gameRefresh, 24)
+
+let playerSpeed = 0;
+
+function gameRefresh() {
+    // clears the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    // renders the items in game
+    wolf.render();
+    
+    piggy.render();
+
+    wolfWall.render();
+
+    ground.render();
+
+    gravity();
+
+    piggy.x += playerSpeed;
+
+}
 
 
+document.onkeydown = keyDown;
+document.onkeyup = keyUp;
 
+function keyDown(e) {
+    switch(e.key) {
+        case('d'):
+            playerSpeed = 3 * (60/fps); //* (60/fps)
+            break;
+        case('a'):
+            playerSpeed = -3 * (60/fps); //* (60/fps)
+            break;
+    }
 
+}
+
+function keyUp(e) {
+    switch(e.key) {
+        case('d'):
+            playerSpeed = 0;
+            break;
+        case('a'):
+            playerSpeed = 0; //* (60/fps)
+            break;
+    }
+}
 
