420c5e0a988e1babe39be66d50f06cb1ae042acf
brandon-w1205 <bwu1205@gmail.com>
2022-08-19 14:02:16 -0700

    Adds font styling, Canvas height/width support, and formatting with canvas included

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const ctx = canvas.getContext('2d');
 // set the canvas's resolution to be the same as the window
 canvas.setAttribute('height', getComputedStyle(canvas)['height'])
 canvas.setAttribute('width', getComputedStyle(canvas)['width'])
+
+// adds a function that will drawboxes upon variable input
+function drawBox(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height)
+}
+
+canvas.addEventListener('click', e => {
+    console.log(e.offsetX, e.offsetY);
+    drawBox(e.offsetX, e.offsetY, 30, 30, 'blue');
+})
