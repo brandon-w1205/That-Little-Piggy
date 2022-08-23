8ea168616221e60c1179cc5cbc705b53090daa38
brandon-w1205 <bwu1205@gmail.com>
2022-08-22 17:35:27 -0700

    adds forks and hit detection for them. adds hearts and hit detection for them

--- script.js
+++ script.js
@@ -104,6 +104,7 @@ class Wolf extends Box {
     constructor(x, y, width, height, color) {
         super(x, y, width, height, color)
         this.health = 100;
+        this.iFrames = false;
     }
 }
 
@@ -119,7 +120,7 @@ let platform1 = new Platform(1171, 481, 400, 20, 'blue');
 
 let platform2 = new Platform(1571, 225, 400, 20, 'blue');
 
-let knife = new Attack(1571, 630, 200, 20, 'darkgrey', 1);
+// let knives = new Attack(1571, 630, 200, 20, 'darkgrey', 1);
 
 function playerMovement() {
     if(keys.right.press && piggy.x < 1465) {
@@ -148,27 +149,60 @@ let bullets = [];
 
 let knivesArr = [];
 setInterval(() => {
-    // knife low
+    // knives low
     knivesArr.push(new Attack(1571, 630, 200, 20, 'darkgrey', 1))
 }, 3000)
 
 setInterval(() => {
-    // knife mid
+    // knives mid
     knivesArr.push(new Attack(1571, 400, 200, 20, 'darkgrey', 1))
 }, 4000)
 
 setInterval(() => {
-    // knife top
+    // knives top
     knivesArr.push(new Attack(1571, 150, 200, 20, 'darkgrey', 1))
 }, 5000)
 
-function detectHitRight(attack, entity) {
+
+
+let forksArr = [];
+
+setInterval(() => {
+    // forks
+    forksArr.push(new Attack(80, -100, 20, 100, 'darkgrey', 1))
+}, 2500)
+
+setInterval(() => {
+    // forks
+    forksArr.push(new Attack(373, -100, 20, 100, 'darkgrey', 1))
+}, 3000)
+
+setInterval(() => {
+    // forks
+    forksArr.push(new Attack(745, -100, 20, 100, 'darkgrey', 1))
+}, 5500)
+
+setInterval(() => {
+    // forks
+    forksArr.push(new Attack(1118, -100, 20, 100, 'darkgrey', 1))
+}, 5000)
+
+setInterval(() => {
+    // forks
+    forksArr.push(new Attack(1491, -100, 20, 100, 'darkgrey', 1))
+}, 2000)
+
+
+
+
+
+
+function detectHit(attack, entity) {
     const right = entity.x + entity.width >= attack.x
     const left = entity.x <= attack.x + attack.width
     const top = entity.y + entity.height >= attack.y
     const bottom = entity.y <= attack.y + attack.height
-    return  (right && left && top && bottom)
-
+    return (right && left && top && bottom)
 }
 
 function wolfHitDetect(attack, entity) {
@@ -206,11 +240,15 @@ function gameRefresh() {
     }
 
     for(let i = 0; i < bullets.length; i += 50)  {
-        bullets[i].render()
+        bullets[i].render();
         bullets[i].x += 3; 
         
-        if(wolfHitDetect(bullets[i], wolf) === true) {
+        if(wolfHitDetect(bullets[i], wolf) === true && wolf.iFrames == false) {
             wolf.health -= bullets[i].attackPoints
+            wolf.iFrames = true;
+            setTimeout (() => {
+                wolf.iFrames = false;
+            }, 200)
             console.log(wolf.health)
         }
     }
@@ -219,7 +257,7 @@ function gameRefresh() {
     
 
     for(let j = 0; j < platformArr.length; j++) {
-        platformArr[j].render()
+        platformArr[j].render();
         platformArr[j].x -= 2.3;
         // platform1 collision detection (remember that the y + height gets added with the velocity which is why the second && statement is required)
         if(piggy.y + piggy.height <= platformArr[j].y && piggy.y + piggy.height + piggy.velocity.y >= platformArr[j].y && piggy.x + piggy.width > platformArr[j].x && piggy.x < platformArr[j].x + platformArr[j].width) {
@@ -229,27 +267,43 @@ function gameRefresh() {
 
     
 
-    
+    if(wolf.health >= 76) {
+        for(let k = 0; k < knivesArr.length; k++) {
+            knivesArr[k].render()
+            knivesArr[k].x -= 2;
+            if(detectHit(knivesArr[k], piggy) === true && piggy.iFrames == false) {
+                piggy.health -= knivesArr[k].attackPoints
+                piggy.iFrames = true;
+                piggy.color = 'white';
+                setTimeout (() => {
+                    piggy.iFrames = false;
+                    piggy.color = 'pink';
+                }, 4000)
+                console.log(piggy.health)
+            }
+        }
+    }
 
-    for(let k = 0; k < knivesArr.length; k++) {
-        knivesArr[k].render()
-        knivesArr[k].x -= 2;
-        if(detectHitRight(knivesArr[k], piggy) === true && piggy.iFrames == false) {
-            piggy.health -= knivesArr[k].attackPoints
+    for(let l = 0; l < forksArr.length; l++) {
+        forksArr[l].render();
+        forksArr[l].y += 2;
+        if(detectHit(forksArr[l], piggy) === true && piggy.iFrames == false) {
+            piggy.health -= forksArr[l].attackPoints;
             piggy.iFrames = true;
             piggy.color = 'white';
-            setTimeout (() => {
+            setTimeout(() => {
                 piggy.iFrames = false;
                 piggy.color = 'pink';
             }, 4000)
             console.log(piggy.health)
-        } //else if(detectHitRight(knivesArr[k], piggy) === false && piggy.iFrames === true) {
-            
-        // }
+        }
     }
 
     
-    
+    if(piggy.health == 0) {
+        clearInterval(refresh, 0);
+    }
+
     
     if(wolf.health == 0){
         clearInterval(refresh, 0);
